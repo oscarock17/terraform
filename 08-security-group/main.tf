a3ed6d3db10fd38e593c0f461f0211a9fbd9d5ec
Oscar Diaz <oscarock02@gmail.com>
2025-07-18 16:19:29 -0500

    :sparkles: Agrega ejercicio de creacion de security groups en AWS

---
+++ 08-security-group/main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = "us-east-1"
+  profile = "dev"
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP and SSH"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "WebSecurityGroup"
+  }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
